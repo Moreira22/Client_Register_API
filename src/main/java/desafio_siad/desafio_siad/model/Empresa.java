cc1d4d6bcdd06f56b787ebf0f869dbe1536ac516
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-08 15:53:48 -0300

    feat: add intidade Emprea

--- src/main/java/desafio_siad/desafio_siad/model/Empresa.java
+++ src/main/java/desafio_siad/desafio_siad/model/Empresa.java
@@ -1,5 +1,46 @@
 package desafio_siad.desafio_siad.model;
 
-public class Empresa {
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Empresa {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+
+    @Column( length = 200, nullable = false, name = "nome")
+    private String nome;
+
+    @Column
+    private Boolean active;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "empresa")
+    private List<Cliente> cliente = new ArrayList<>();
 }
