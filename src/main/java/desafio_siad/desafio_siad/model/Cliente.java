df84f6b4b4e5958572313bed9cfa7f67874a56a9
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-08 15:54:01 -0300

    feat: add intidade Cliente

---
+++ src/main/java/desafio_siad/desafio_siad/model/Cliente.java
@@ -0,0 +1,51 @@
+package desafio_siad.desafio_siad.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+
+    @Column( length = 200, nullable = false, name = "nome")
+    private String nome;
+
+    @Column
+    private Data data_nacimento;
+
+    @Column
+    private Boolean active;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "empresa_id", nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Empresa empresa;
+
+}
