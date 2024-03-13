cdfb80a639ca087877261130fb4cd295369d4d5f
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-13 09:54:07 -0300

    feat: add JuriticoController

---
+++ src/main/java/desafio_siad/desafio_siad/controller/JuriticoContrller.java
@@ -0,0 +1,35 @@
+package desafio_siad.desafio_siad.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import desafio_siad.desafio_siad.model.Juridico;
+import desafio_siad.desafio_siad.repository.JuriticoResposity;
+import lombok.AllArgsConstructor;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+@RequestMapping("/clienteJuritico")
+@AllArgsConstructor
+public class JuriticoContrller {
+    @Autowired
+    private final JuriticoResposity juriticoResposity;
+    @GetMapping("/All")
+    public @ResponseBody List<Juridico> getALL() {
+       return juriticoResposity.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Juridico> getById(@PathVariable Long id){
+        return juriticoResposity.findById(id)
+        .map(recordFoumd -> ResponseEntity.ok().body(recordFoumd))
+        .orElse(ResponseEntity.notFound().build());
+    }
+}
