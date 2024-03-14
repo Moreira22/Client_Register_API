3339c0cf3bc6e9581db5a58129379b14fb26840a
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-14 11:28:53 -0300

    fix: add PUT in JuriticoController

--- src/main/java/desafio_siad/desafio_siad/controller/JuriticoContrller.java
+++ src/main/java/desafio_siad/desafio_siad/controller/JuriticoContrller.java
@@ -1,24 +1,32 @@
 package desafio_siad.desafio_siad.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import desafio_siad.desafio_siad.domin.juritico.JuriticoRequestDTO;
+import desafio_siad.desafio_siad.model.Empresa;
 import desafio_siad.desafio_siad.model.Juridico;
 import desafio_siad.desafio_siad.repository.ContatoRepository;
 import desafio_siad.desafio_siad.repository.JuriticoResposity;
-import lombok.AllArgsConstructor;
-import java.util.List;
 
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -48,6 +56,20 @@ public class JuriticoContrller {
         Juridico newJuridico = new Juridico(data);
         juriticoResposity.save(newJuridico);
         return ResponseEntity.status(HttpStatus.CREATED).body(newJuridico);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Juridico> updateJuritico(@PathVariable long id, @RequestBody JuriticoRequestDTO data) {
+        Optional<Juridico> optionalJuritico = juriticoResposity.findById(id);
+        if(optionalJuritico.isPresent()){
+            Juridico juridico = optionalJuritico.get();
+            juridico.setNome(data.nome());
+            juridico.setData_nacimento(data.data_nacimento());
+            juridico.setCnpf(data.cnpf());
+            juridico.setIe(data.ie());
+            return ResponseEntity.status(HttpStatus.OK).body(juridico);
+        }
+        return ResponseEntity.noContent().build();
     }
     
 }
