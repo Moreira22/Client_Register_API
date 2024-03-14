a544ab8b420dbe88a1a633deff972dfb14695dd8
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-14 09:45:02 -0300

    fix: add Post in ClienteController

--- src/main/java/desafio_siad/desafio_siad/domin/cliente/ClienteRequestDTO.java
+++ src/main/java/desafio_siad/desafio_siad/domin/cliente/ClienteRequestDTO.java
@@ -4,8 +4,16 @@ import desafio_siad.desafio_siad.model.Empresa;
 
 public record ClienteRequestDTO(
     Long id,
-    String nomoe,
+    String nome,
+    Boolean active,
     String data_nacimento,
+    String cpf,
+    String uf,
+    String cidade,
+    String bairro,
+    Integer numero,
+    String cnpf,
+    String ie,
     Empresa empresa
 ) {
 
