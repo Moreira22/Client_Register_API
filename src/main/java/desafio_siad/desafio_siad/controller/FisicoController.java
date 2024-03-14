1407483de57278a33aea944fb19bf9a3bc18c7f8
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-14 11:29:21 -0300

    fix: add PUT in FisicoController

--- src/main/java/desafio_siad/desafio_siad/controller/FisicoController.java
+++ src/main/java/desafio_siad/desafio_siad/controller/FisicoController.java
@@ -1,23 +1,29 @@
 package desafio_siad.desafio_siad.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import desafio_siad.desafio_siad.domin.fisico.FisicoRequestDTO;
 import desafio_siad.desafio_siad.model.Fisico;
 import desafio_siad.desafio_siad.repository.FisicoRepository;
-import lombok.AllArgsConstructor;
-import java.util.List;
 
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -39,12 +45,31 @@ public class FisicoController {
         .map(recordFoumd -> ResponseEntity.status(HttpStatus.OK).body(recordFoumd))
         .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
+
     @PostMapping("/register")
     public ResponseEntity<Fisico> postRegisterFisico(@RequestBody FisicoRequestDTO data) {
         Fisico newFisico = new Fisico(data);
             fisicoRepository.save(newFisico);
             return ResponseEntity.status(HttpStatus.CREATED).body(newFisico);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fisico> updateFisico(@PathVariable Long id, @RequestBody FisicoRequestDTO data) {
+        Optional<Fisico> optionalFisico = fisicoRepository.findById(id);
+        if(optionalFisico.isPresent()){
+            Fisico fisico = optionalFisico.get();
+            fisico.setNome(data.nome());
+            fisico.setData_nacimento(data.data_nacimento());
+            fisico.setCpf(data.cpf());
+            fisico.setUf(data.uf());
+            fisico.setCidade(data.cidade());
+            fisico.setBairro(data.bairro());
+            fisico.setNumero(data.numero());
+            return ResponseEntity.status(HttpStatus.OK).body(fisico);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
     
 
 }
