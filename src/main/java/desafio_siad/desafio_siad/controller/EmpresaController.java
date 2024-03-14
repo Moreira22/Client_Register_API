a6b2001a088c99cc2a3d9ecf714f3705bd154609
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-03-14 09:44:09 -0300

    fix: add Post in EmpresaController

--- src/main/java/desafio_siad/desafio_siad/controller/EmpresaController.java
+++ src/main/java/desafio_siad/desafio_siad/controller/EmpresaController.java
@@ -2,6 +2,7 @@ package desafio_siad.desafio_siad.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import desafio_siad.desafio_siad.domin.empresa.EmpresaResquestDTO;
 import desafio_siad.desafio_siad.model.Empresa;
 import desafio_siad.desafio_siad.repository.EmpresRepository;
 import lombok.AllArgsConstructor;
@@ -9,12 +10,16 @@ import lombok.AllArgsConstructor;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/empresa")
@@ -25,8 +30,9 @@ public class EmpresaController {
     private final EmpresRepository empresRepository;
 
     @GetMapping("/All")
-    public @ResponseBody List<Empresa> getALL() {
-       return empresRepository.findAll();
+    public ResponseEntity<List<Empresa>> getALL() {
+        var allempresa = empresRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(allempresa);
     }
 
     @GetMapping()
@@ -37,9 +43,18 @@ public class EmpresaController {
     @GetMapping("/{id}")
     public ResponseEntity<Empresa> getById(@PathVariable Long id){
         return empresRepository.findById(id)
-        .map(recordFoumd -> ResponseEntity.ok().body(recordFoumd))
-        .orElse(ResponseEntity.notFound().build());
+        .map(recordFoumd -> ResponseEntity.status(HttpStatus.OK).body(recordFoumd))
+        .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
+    
+    @PostMapping("/register")
+    public ResponseEntity<Empresa> posRegisterEmpresa(@RequestBody EmpresaResquestDTO data){
+        Empresa newempresa = new Empresa(data);
+        empresRepository.save(newempresa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newempresa);
+    }
+    
+    
     
     
 
